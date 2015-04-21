a728da74200852bd7288799296d3adf9785e61a8
Chizhezvskaya <nastenka112004@mail.ru>
2015-04-21 20:51:28 +0300

    Create The Most Numbers.py

---
+++ The Most Numbers.py
@@ -0,0 +1,9 @@
+def checkio(*args):
+    if len(args) == 0:
+        return 0
+    return max(args) - min(args)
+
+    assert almost_equal(checkio(1, 2, 3), 2, 3), "3-1=2"
+    assert almost_equal(checkio(5, -5), 10, 3), "5-(-5)=10"
+    assert almost_equal(checkio(10.2, -2.2, 0, 1.1, 0.5), 12.4, 3), "10.2-(-2.2)=12.4"
+    assert almost_equal(checkio(), 0, 3), "Empty"
